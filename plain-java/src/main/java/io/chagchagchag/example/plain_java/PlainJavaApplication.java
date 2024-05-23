c3c118f7ec50744bd914c7cf1c190d25a304d22f
chagchagchag <chagchagchag.dev@gmail.com>
2024-05-23 22:41:58 +0900

    first commit

---
+++ plain-java/src/main/java/io/chagchagchag/example/plain_java/PlainJavaApplication.java
@@ -0,0 +1,13 @@
+package io.chagchagchag.example.plain_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlainJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlainJavaApplication.class, args);
+	}
+
+}
